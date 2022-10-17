76439d9bbbb4df558a6257e38b21f973db322a4f
DariaKorzeniewska <162806@student.uwm.edu.pl>
2022-10-17 13:34:03 +0200

    new17.10

--- lab2/macierze.py
+++ lab2/macierze.py
@@ -1,7 +1,37 @@
 import numpy as np
-A = np.array([[5,10],[7,6]])
+A = np.array([[5, 10], [7, 6]])
+B = np.array([[2, 3], [1, 9]])
 # zad1
 def sum(a,b):
+    return a+b
+
+sum1 = sum(A,B)
+print(sum1)
+
+# zad2
+def product(a,b):
+    return a*b
+
+product1 = product(A,B)
+print(product1)
+
+# zad3
+def mult(a,x):
+    return a*x
+
+mult1 = mult(A,2)
+print(mult1)
+
+# zad4
+C = np.array([[1,2], [3,4]])
+
+def transp(a):
+    return a.transpose()
+
+transp1 = transp(C)
+print(transp1)
+
+
 
 
 
