f1f3561e6a037c6cf721759035b1462dff833470
DariaKorzeniewska <162806@student.uwm.edu.pl>
2022-11-07 13:25:22 +0100

    zad1

---
+++ lab4/zad1.py
@@ -0,0 +1,112 @@
+def nwd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+class Wymierna:
+    p = 0
+    q = 1
+    def __init__(self, p: int, q: int) -> None:
+        self.p = p
+        self.q = q
+
+    def get_licznik(self):
+        return self.p
+
+    def get_mianownik(self):
+        return self.q
+
+    def __repr__(self):
+        return str(self.p) + "/" + str(self.q)
+
+    def __float__(self):
+        return float(self.p/self.q)
+
+    def __add__(self, a, b):
+        a *= self.q
+        new1 = self.p * b
+        new2 = self.q * b
+        new1 += a
+        dzielnik = nwd(new1, new2)
+        new1 /= dzielnik
+        new2 /= dzielnik
+        return int(new1), int(new2)
+
+    def __sub__(self, a, b):
+        a *= self.q
+        new1 = self.p * b
+        new2 = self.q * b
+        new1 -= a
+        dzielnik = nwd(new1, new2)
+        new1 /= dzielnik
+        new2 /= dzielnik
+        return int(new1), int(new2)
+
+    def __eq__(self, a, b):
+        if self.q == b:
+            if self.p == a:
+                return True
+        return False
+
+    def __ne__(self, a, b):
+        return not self.__eq__(a, b)
+
+    def __lt__(self, a, b):  # less than
+        a *= self.q
+        self.p *= b
+        self.q *= b
+        b *= self.q
+
+        if self.p < a:
+            if self.q < b:
+                return True
+        return False
+
+    def __le__(self, a, b):  # less or equal
+        a *= self.q
+        self.p *= b
+        self.q *= b
+        b *= self.q
+
+        if self.p <= a:
+            if self.q <= b:
+                return True
+        return False
+
+    def __gt__(self, a, b):  # greater than
+        return not self.__le__(a, b)
+
+    def __ge__(self, a, b):  # greater or equal
+        return not self.__lt__(a, b)
+
+l1 = 1
+m1 = 2
+l2 = 3
+m2 = 10
+
+w1 = Wymierna(l1, m1)
+w2 = Wymierna(l2, m2)
+
+print(w1.get_licznik())
+print(w1.get_mianownik())
+print(w2.get_licznik())
+print(w2.get_mianownik())
+
+print(w1)
+print(w2)
+
+print(w1.__float__())
+print(w2.__float__())
+
+print(w1.__add__(w2.p, w2.q))
+
+print(w1.__sub__(w2.p, w2.q))
+
+print(w1.__eq__(w2.p, w2.q))
+print(w1.__ne__(w2.p, w2.q))
+print(w1.__lt__(w2.p, w2.q))
+print(w1.__le__(w2.p, w2.q))
+print(w1.__gt__(w2.p, w2.q))
+print(w1.__ge__(w2.p, w2.q))
+
+
